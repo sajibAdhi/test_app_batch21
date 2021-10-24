4318d112d4512bd79a2de09771d014da5be5d91a
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-10-25 01:17:27 +0600

    email_text_form_field stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_app_batch21/screens/auth/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: LoginPage(),
     );
   }
 }
--- lib/widgets/inputs/email_text_form_field.dart
+++ lib/widgets/inputs/email_text_form_field.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:test_app_batch21/validators/email_validator.dart';
 import 'package:test_app_batch21/widgets/inputs/underline_input_border_colors.dart';
 
-class EmailTextFromField extends StatelessWidget {
-  final TextEditingController? controller;
-  final String? initialValue;
+class EmailTextFromField extends StatefulWidget {
+  final TextEditingController controller;
+  final String initialValue;
   final String? hintText;
   final FocusNode? focusNode;
   final TextInputAction? textInputAction;
@@ -12,8 +12,8 @@ class EmailTextFromField extends StatelessWidget {
 
   const EmailTextFromField(
       {Key? key,
-      this.controller,
-      this.initialValue,
+      required this.controller,
+      required this.initialValue,
       this.hintText,
       this.focusNode,
       this.onEditingComplete,
@@ -21,21 +21,26 @@ class EmailTextFromField extends StatelessWidget {
       : super(key: key);
 
   @override
+  _EmailTextFromFieldState createState() => _EmailTextFromFieldState();
+}
+
+class _EmailTextFromFieldState extends State<EmailTextFromField> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: TextFormField(
-        controller: controller,
-        initialValue: initialValue,
-        focusNode: focusNode,
-        textInputAction: textInputAction,
-        onEditingComplete: onEditingComplete,
+        controller: widget.controller,
+        initialValue: widget.initialValue,
+        focusNode: widget.focusNode,
+        textInputAction: widget.textInputAction,
+        onEditingComplete: widget.onEditingComplete,
         validator: (value) => emailValidator(value),
         keyboardType: TextInputType.emailAddress,
         style: TextStyle(
           color: Colors.white,
         ),
         decoration: InputDecoration(
-          hintText: hintText,
+          hintText: widget.hintText,
           hintStyle: TextStyle(
             color: Colors.white,
           ),
